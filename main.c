e358f0fffbb3ae19487ee5f78d7ff5620760c38b
Waznygx <135837095+Waznygx@users.noreply.github.com>
2023-09-23 21:17:23 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,313 +1,96 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-//#include<stdlib.h>
-//typedef struct MyStruct
-//{
-//	int i;
-//	int arr[];
-//}MyStruct;
-//int main()
-//{
-//	MyStruct* p=(MyStruct*)malloc(sizeof(MyStruct) + 10 * sizeof(int));
-//	p->i = 10;
-//	int i = 0;
-//	for ( i = 0; i < 10; i++)
-//	{
-//		p->arr[i] = i;
-//	}
-//	MyStruct* ptr=(MyStruct*) realloc(p, sizeof(MyStruct) * 10);
-//	if (ptr != NULL)
-//	{
-//		p = ptr;
-//	}
-//	free(p);
-//	p = NULL;
-//	return 0;
-//}
 
-//多组输入：输入一个数字，输出“X”形图案，其中图案两条对角线的任何一条的*的个数等于所输入的数字
-//int main()
+////输入任意两个数，在一个子函数中求两个数的最大公约数和最小公倍数，并将结果返回
+////另外一个子函数判断其最大公约数的奇偶性，在主函数输出结果
+////把主函数和两个子函数放在不同的目录下，编译时用-I选项包含子函数的路径
+//#include <stdio.h>
+//#include <math.h>
+//#include <assert.h>
+//int max_jiou(int gys)
 //{
-//	int input = 0;
-//	printf("input:");
-//	while (scanf("%d", &input) != EOF)
-//		//当从键盘读取一个整数并将其存储在变量 input 中成功时，就执行循环体中的代码。
-//		//scanf 函数会返回成功读取的数据项数，如果读取失败或遇到文件结束符（EOF），则返回 EOF
+//	if (gys / 2 == 0)
 //	{
-//		int i = 0;//行
-//		int j = 0;//列
-//		for ( i = 0; i < input; i++)
-//		{
-//			for (j = 0; j < input; j++)
-//			{
-//				if (i == j || (i+j == input - 1))
-//				{
-//					printf("*");
-//				}
-//				else
-//				{
-//					printf("·");
-//				}
-//			}
-//			printf("\n");
-//		}
+//		return 0;
 //	}
-//	return 0;
+//	return 1;
 //}
-
-//公务员考试：键盘输入若干组成绩，每组7个分数（百分制），去掉一个最高分和最低分，输出每组的平均成绩
-//输入：7个整数(0-100)代表7个成绩，用空格隔开
-//输出：去掉最高分和最低分的平均成绩，小数点保留后两位，每行输出后换行
-//99 45 78 67 72 88 60
-//73.00
-//// 方法二
-//int main()
+//void max_min(int a, int b, int* gys, int* gbs)
 //{
-//	int input = 0;
-//	int i = 0;
-//	int sum = 0;
-//	int min = 100;
+//	int pa = a;
+//	int pb = b;
 //	int max = 0;
-//	for ( i = 0; i < 7; i++)
+//	assert(pa && pb);
+//	if (pa < pb)
 //	{
-//		scanf("%d", &input);
-//		sum += input;
-//		if (max < input)
-//		{
-//			max = input;
-//		}
-//		if (min > input)
-//		{
-//			min = input;
-//		}
+//		int temp = pa;
+//		pa = pb;
+//		pb = temp;
 //	}
-//	sum -= (min + max);
-//	printf("%.2f ", sum/5.0);
-//	
-//	return 0;
-//}
-//// 方法一
-//int main() {
-//    int scores[7];
-//    while (scanf("%d %d %d %d %d %d %d", &scores[0], &scores[1], &scores[2], &scores[3], &scores[4], &scores[5], &scores[6]) == 7) {
-//        int max = scores[0];
-//        int min = scores[0];
-//        double sum = 0;
-//        for (int i = 0; i < 7; i++) 
-//        {
-//            if (scores[i] > max) 
-//            {
-//                max = scores[i];
-//            }
-//            if (scores[i] < min) 
-//            {
-//                min = scores[i];
-//            }
-//            sum += scores[i];
-//        }
-//        sum -= max + min;
-//        printf("%.2f\n", sum / 5.0);
-//    }
-//    return 0;
-//}
-
-//输入年份和月份，输出这个月有多少天
-//输入：2008 2
-//输出：29
-//int main()
-//{
-//	int year = 0;
-//	int month = 0;
-//	int arr1[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };//为了使得下标和月份对齐
-//	printf("input:");
-//	while (scanf("%d %d", &year, &month) != EOF)
+//	while (max = pa % pb)
 //	{
-//		int day = arr1[month];
-//		if (month = 2)
-//		{
-//			if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
-//			{
-//				day = arr1[month] + 1;
-//			}
-//		}
-//		printf("%d", day);
+//		pa = pb;
+//		pb = max;
 //	}
-//	return 0;
+//	*gys = pb;
+//	*gbs = a * b / (*gys);//两个整数的乘积=它们的最大公约数和最小公倍数的乘积
 //}
-
-////有一个有序数字序列，从小到大排序，将一个新输入的数插入到序列中，保证插入新数后，序列仍然是升序
-////输入描述:第一行输入一个整数(0<=N<=50)。第二行输入N个升序排列的整数，输入用空格分隔的N个整数。第三行输入想要进行插入的一个整数
-////输出描述:输出为一行，N + 1个有序排列的整数
-////输入:5
-////     1 6 9 22 30
-////     8 
-////输出:1 6 8 9 22 30
 //int main()
-//{
-//	int num = 0;
-//    int input[51] = { 0 };
-//	int insert = 0;
-//	int i = 0;
-//	int j = 0;
-//	scanf("%d", &num);
-//	for ( i = 0; i < num; i++)
-//	{
-//		scanf("%d ", &input[i]);
-//	}
-//	scanf("%d", &insert);
-//    //方法二
-//    for (i = num - 1; i >= 0; i--)
-//    {
-//        if (input[i] > insert)
-//        {
-//            input[i + 1] = input[i];
-//        }
-//        else
-//        {
-//            input[i + 1] = insert;
-//            break;
-//        }
-//    }
-//    //当插入的数据小于所有数据时
-//    input[i+1] = insert;
-//    /////方法一
-//    //// 插入新的数字并保持数组有序
-//    //for (i = 0; i < num; i++)
-//    //{
-//    //    if (insert < input[i])
-//    //    {
-//    //        for (j = num; j > i; j--)//将数组中的元素向右移动一位，为新插入的元素腾出空间。
-//    //        {
-//    //            input[j] = input[j - 1];
-//    //        }
-//    //        input[i] = insert;
-//    //        break;
-//    //    }
-//    //}
-//    //// 如果新的数字是最大的，那么它应该被添加到数组的末尾
-//    //if (i == num)
-//    //{
-//    //    input[num] = insert;
-//    //}
-//    // 打印结果
-//    for (i = 0; i <= num; i++)
-//    {
-//        printf("%d ", input[i]);
-//    }
-//	return 0;
-//}
-
-//int main()
-//{
-//	FILE* pf = fopen("C:\\Users\\张艺兴的圈外女友\\source\\repos\\Project5\\test.txt", "r");
-//	if (pf == NULL)
-//	{
-//		perror("fopen");
-//		return 1;
-//	}
-//	fputs("hello world", pf);
-//	int ret = fgetc(pf);
-//	printf("%c\n", ret);
-//	 ret = fgetc(pf);
-//	printf("%c\n", ret);
-//	 ret = fgetc(pf);
-//	printf("%c\n", ret);
-//	 ret = fgetc(pf);
-//	printf("%c\n", ret);
-//	ret = fgetc(pf);
-//	printf("%c\n", ret);
-//	fclose(pf);
-//	pf = NULL;
-//	return 0;
-//}
-
-//int main()
-//{
-//	int ret = fgetc(stdin);
-//	printf("%c\n", ret);
-//	ret = fgetc(stdin);
-//	printf("%c\n", ret);
-//	ret = fgetc(stdin);
-//	printf("%c\n", ret);
-//	ret = fgetc(stdin);
-//	printf("%c\n", ret);
-//	ret = fgetc(stdin);
-//	printf("%c\n", ret);
-//	return 0;
-//}
-
-//int main()
-//{
-//	char arr[40] = {0};
-//	FILE* pf = fopen("C:\\Users\\张艺兴的圈外女友\\source\\repos\\Project5\\test.txt", "r");
-//	if (pf == NULL)
-//	{
-//		perror("fopen");
-//		return 1;
-//	}
-//	//fputs("hello world", pf); 
-//	fgets(arr, 5, pf);
-//	printf("%s", arr);
-//
-//	fclose(pf);
-//	pf = NULL;
-//	return 0;
-//}
-
-#include<string.h>
-//void add()
 //{
 //	int a = 0;
 //	int b = 0;
-//	printf("input:");
+//	printf("please input two numbers:");
 //	scanf("%d%d", &a, &b);
-//	printf("%d", a + b);
-//}
-////void fact(int a, int b)
-////{
-////	printf("%d", a * b);
-////}
-//int main()
-//{
-//	char input[50] = {0};
-//	printf("input:");
-//	scanf("%s", input);
-//	if (strcmp(input,"123")==0)
+//
+//	//调用最大公约数/最小公倍数函数
+//	int gys = 0;
+//	int gbs = 0;
+//	max_min(a, b, &gys, &gbs);
+//	printf("max_gys: %d\n",gys);
+//	printf("min_gbs: %d\n",gbs);
+//
+//	//调用奇偶性函数
+//	int ret = max_jiou(gys);
+//	if (ret == 0)
 //	{
-//		printf("yes\n");
+//		printf("%d is a oushu\n",gys);
 //	}
-//	else if (strcmp(input, "add") == 0)
-//	{
-//		/*printf("input:");
-//		scanf("%d%d", &a, &b);
-//		add(a,b);*/
-//		add();
-//	}
-//	/*else if (strcmp(input, "fact") == 0)
-//	{
-//		printf("input:");
-//		scanf("%d%d", &a, &b);
-//		fact(a, b);
-//	}*/
 //	else
 //	{
-//		printf("error\n");
+//		printf("%d is a jishu\n",gys);
 //	}
-//	
 //	return 0;
 //}
 
+////从键盘输入一个数，判断该数是否为素数，要求判断过程用子函数完成，在主函数中调用该函数并显示输出
+////在运行中添加-E,-C,-S,-c,-o等选项，生成i，s，o等文件，掌握及其用法
+//#include<stdio.h>
+//#include<math.h>
+//int is_prime(int input)
+//{
+//	int i = 0;
+//	for ( i = 2; i <= sqrt(input); i++)
+//	{
+//		if (input % i == 0)
+//		{
+//			return 0;
+//			break;
+//		}
+//	}
+//	return 1;
+//}
 //int main()
 //{
-//	int a = 0;
-//	int b = 0;
-//	int sum = 0;
-//	int i = 0;
-//	scanf("%d %d", &a, &b);
-//	for ( i = a; i <= b; i++)
+//	int input = 0;
+//	printf("please input a num:");
+//	scanf("%d", &input);
+//	int ret = is_prime(input);
+//	if (ret == 0)
 //	{
-//		sum += i;
+//		printf("%d is not a prime\n",input);
 //	}
-//	printf("%d", sum);
+//	else
+//	{
+//		printf("%d is a prime\n",input);
+//	}
 //	return 0;
 //}
